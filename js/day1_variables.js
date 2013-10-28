bf043234d21896bed7a151bf4e4d08ea70dd267e
Joseph Dalton <jdalton@fullsail.com>
2013-10-28 11:34:59 -0400

    commentsAndVariables
    Class 1 comments and variables introduction. Strings only.

---
+++ js/day1_variables.js
@@ -0,0 +1,21 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: mdvn
+ * Date: 10/28/13
+ * Time: 10:48 AM
+ * To change this template use File | Settings | File Templates.
+ */
+ // Comment type 1
+
+/* comment type 2 */
+
+
+var name;
+name ='Joseph Dalton';
+
+console.log(name);
+
+
+
+
+
