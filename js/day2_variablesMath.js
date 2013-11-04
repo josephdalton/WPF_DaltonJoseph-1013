ddc7090c7257d42d6294732bbd1000b455213e47
Joseph Dalton <jdalton@fullsail.com>
2013-11-04 08:53:18 -0500

    day3

---
+++ js/day2_variablesMath.js
@@ -0,0 +1,52 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: mdvn
+ * Date: 10/30/13
+ * Time: 9:39 AM
+ * To change this template use File | Settings | File Templates.
+ */
+//Math class
+//Expressions
+//String Manipulation
+
+
+//console.log(4%2);
+
+
+////arrays
+//var shoppingListArray =['Bread','Oil','Soap'];
+////console.log(shoppingListArray[1]);
+//shoppingListArray[3]='Toothpaste';
+//shoppingListArray.push('Grapes');
+//shoppingListArray.pop();
+//shoppingListArray.slice(2,2);
+
+//console.log(shoppingListArray[3]);
+
+//var numberArray =[1,3,5,2,23,52,167];
+//
+//console.log(Math.max(1,2,4,6));
+//console.log(Math.max.apply(null,numberArray));
+//console.log(Math.min(2,3,5,23));
+//console.log(Math.round(2334242.1342143252));
+//console.log(Math.ceil(23.0000001));
+//console.log(Math.floor(23.999999));
+//pathagorus theorum
+var triangle = [];
+triangle[0]=Number(prompt('base?'));
+triangle[1]=Number(prompt('height?'));
+triangle[2]=Math.sqrt(triangle[0]*triangle[0]
++triangle[1]*triangle[1]
+);
+
+console.log(
+
+    triangle
+);
+
+
+
+
+
+
+
---
+++ js/day3_ArraysLoops.js
@@ -0,0 +1,82 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: mdvn
+ * Date: 11/1/13
+ * Time: 9:05 AM
+ * To change this template use File | Settings | File Templates.
+ */
+//Arrays of Arrays
+//var arrayLayerOne = [];
+//arrayLayerOne[0]=[];
+//arrayLayerOne[1]=[];
+//arrayLayerOne[2]=[];
+//
+//arrayLayerOne[0][0]='Dog';
+//arrayLayerOne[0][1]='Cat';
+//arrayLayerOne[0][2]='Bird';
+//
+//arrayLayerOne[1][0]=1;
+//arrayLayerOne[1][1]=10;
+//arrayLayerOne[1][2]=908277.28827;
+//
+//arrayLayerOne[2][0]=['Dog','monkey'];
+//arrayLayerOne[2][1]=['Cat','dolphin'];
+//arrayLayerOne[2][2]=['Bird','whale'];
+//
+//console.log(arrayLayerOne[0][0]);
+//console.log(arrayLayerOne[2][0]);
+//console.log(arrayLayerOne[2][0][0]);
+
+//Creating a multiplication table. which is then multipled by a prompted value
+//var pro=parseInt(prompt('Give me an integer'));
+//var arr =[];
+//arr[0]=[];
+//arr[1]=[];
+//arr[2]=[];
+//arr[3]=[];
+//
+//arr[0][0]=1;
+//arr[0][1]=2;
+//arr[0][2]=3;
+//arr[0][3]=4;
+//
+//arr[1][0]=2;
+//arr[2][0]=3;
+//arr[3][0]=4;
+//
+//arr[1][1]=arr[0][1]*arr[1][0]*pro;
+//arr[1][2]=arr[0][2]*arr[1][0]*pro;
+//arr[1][3]=arr[0][3]*arr[1][0]*pro;
+//
+//arr[2][1]=arr[0][1]*arr[2][0]*pro;
+//arr[2][2]=arr[0][2]*arr[2][0]*pro;
+//arr[2][3]=arr[0][3]*arr[2][0]*pro;
+//
+//arr[3][1]=arr[0][1]*arr[3][0]*pro;
+//arr[3][2]=arr[0][2]*arr[3][0]*pro;
+//arr[3][3]=arr[0][3]*arr[3][0]*pro;
+//
+//arr[0][0] = arr[0][0]*pro;
+//arr[0][1] = arr[0][1]*pro;
+//arr[0][2] = arr[0][2]*pro;
+//arr[0][3] = arr[0][3]*pro;
+//arr[1][0] = arr[1][0]*pro;
+//arr[2][0] = arr[2][0]*pro;
+//arr[3][0] = arr[3][0]*pro;
+//
+//console.log(arr[0]);
+//console.log(arr[1]);
+//console.log(arr[2]);
+//console.log(arr[3]);
+
+for(i=0;i<=2;i=i+1){
+    for(j=0;j<=2;j++) {
+        console.log('['+i +']['+j+']');
+
+    }
+
+
+
+}
+
+
