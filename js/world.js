6fe07c7ee2cfed931cdcf2b22f14c528a5ede35a
Joseph Dalton <jdalton@fullsail.com>
2013-11-15 09:42:12 -0500

    InClassWorldFunctionsExample

--- js/world.js
+++ js/world.js
@@ -10,25 +10,69 @@
 
 //build a world array to contain our world of random land types.
 
-var world=[];
 
-for(o=0;o<10;o++){
-   world[o]=[];
-   for(i=0;i<10;i++){
-       //I want to choose between 4 random land types
-       var landSelection = Math.ceil(4*Math.random());
-       if(landSelection==1){world[o][i]='Swamp\t\t';}
-       else if(landSelection==2){world[o][i]='Desert\t\t';}
-       else if(landSelection==3){world[o][i]='Mountain\t';}
-       else (world[o][i]='Forest\t\t');
+function createWorld(){
+    var world=[];
 
-   }
+    for(o=0;o<10;o++){
+       world[o]=[];
+       for(i=0;i<10;i++){
+           //I want to choose between 4 random land types
+           var landSelection = Math.ceil(4*Math.random());
+           if(landSelection==1){world[o][i]='Swamp';}
+           else if(landSelection==2){world[o][i]='Desert';}
+           else if(landSelection==3){world[o][i]='Mountain';}
+           else (world[o][i]='Forest');
 
-    console.log(world[o]);
+       }
+    }
+
+    return world;
+
 }
 
 
-
+//console.log(world[o]);
+/*
+ Forest
+ Swamp
+ Desert
+ Mountain
+ */
 //
+var world1= createWorld();
+var world2= createWorld();
+console.log('*****UGLY VERSION*****')
+for(i=0;i<world1.length;i++){
+        console.log(world1[i]);
+  }
+
+
+  function worldDisplay(worldArray){
+      //input world array from the creation
+      //Make this into a formatted grid with even spacing
+      var formattedResult='';
+      for(i=0;i<worldArray.length;i++){
+          for(j=0;j<worldArray[i].length;j++)
+          {
+
+
+
+              formattedResult+= worldArray[i][j] +' ';
+
+          }
+          formattedResult+='\n';
+      }
+
+
+      return formattedResult;
+
+    //output the formatted version of the world array
+  }
+
+console.log(worldDisplay(world1));
+
+
+
 
 
